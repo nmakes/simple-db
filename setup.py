e00138ca71da4b72e409f99ebc364b89d50007ea
nmakes <nav.naveenvenkat@gmail.com>
2017-12-17 22:51:39 +0000

    adding CUI

--- setup.py
+++ setup.py
@@ -6,5 +6,34 @@
 
 from os import system
 
-print "Running git pull ..."
-system('git pull https://github.com/nmakes/simple-db master')
+def menu():
+
+	print "SimpleDB v0.1"
+	print "-------------"
+	print "1. Update"
+	print "2. Copyright Information"
+	print "3. Exit"
+	print
+
+if __name__=='__main__':
+
+	inp = None
+
+	while(inp!=3):
+
+		menu()
+		inp = raw_input(": ")
+
+		if inp.strip()=='1':
+
+			print "Running git pull ..."
+			system('git pull https://github.com/nmakes/simple-db master')
+
+		elif inp.strip()=='2':
+			
+			print "Copyright (c) 2017 Naveen Venkat\nnav.naveenvenkat@gmail.com\nhttps://github.com/nmakes/simple-db"
+			s  = raw_input("\n\nPress enter to continue .. ")
+
+		elif inp.strip() == '3':
+
+			exit()
